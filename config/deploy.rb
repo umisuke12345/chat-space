08790db234bddf99ca780d27cb53ba43b81754a0
umisuke12345 <umisuke0106@icloud.com>
2019-10-21 11:56:21 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.11.2"
 
 set :application, "chat-space"
 set :repo_url, 'git@github.com:umisuke12345/chat-space.git'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user

18c43ff33ecffdcab729ad4071af182126f0124f
umisuke12345 <umisuke0106@icloud.com>
2019-10-21 19:39:05 +0900

    自動更新機能の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,21 +45,21 @@ $(document).on("turbolinks:load", function() {
   })
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    var last_message_id = $('.message:last').data('id');
+    var last_message_id = $('.message:last').data('message_id');
     $.ajax({
       url: "api/messages",
       type: 'get',
       dataType: 'json',
-      data: {id: last_message_id}
+      data: {last_id: last_message_id}
     })
     .done(function(messages) {
       var insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildHTML(message);
           $('.messages').append(insertHTML)
+        })
           $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      })
-    })
+        })
     .fail(function() {
       alert('自動更新に失敗しました');
       });
